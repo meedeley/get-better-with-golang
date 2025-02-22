c043340f13a8da96b8b4df900a6c980717735c32
meedeley <nicholasaputra77@gmail.com>
2025-02-22 22:49:33 +0700

    004

---
+++ 004/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var hello_world, meedeley string
+
+	hello_world = "Hello World"
+	meedeley = "MEEDELEY"
+
+	fmt.Println(hello_world, " ", meedeley)
+}
